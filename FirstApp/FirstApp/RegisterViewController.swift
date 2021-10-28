c5b293032cdbd6cf44001d51d0b12a834d2d3627
Aleksandar Lukic <acafineti@gmail.com>
2021-10-29 00:23:49 +0200

    register feature update

--- FirstApp/FirstApp/RegisterViewController.swift
+++ FirstApp/FirstApp/RegisterViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet private var firstNameTextField: UITextField!
     @IBOutlet private var lastNameTextField: UITextField!
     @IBOutlet private var usernameTextField: UITextField!
@@ -23,6 +23,14 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        usernameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+        passwordTextField.autocorrectionType = .no
+        confirmPasswordTextField.autocorrectionType = .no
         updateTextView()
     }
     
@@ -36,6 +44,8 @@ class RegisterViewController: UIViewController {
         present(destinationVC, animated: true, completion: nil)
     }
     
+    //MARK: - Methods
+    
     func updateTextView () {
         let path = "https://en.wikipedia.org/wiki/Privacy_policy"
         let text = privacyPolicyTextView.text ?? ""
@@ -44,6 +54,51 @@ class RegisterViewController: UIViewController {
         privacyPolicyTextView.attributedText = attributedString
         privacyPolicyTextView.font = font
         privacyPolicyTextView.textAlignment = .center
+    }
+    
+    func loginAlerts(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: Constants.ok, style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func isPasswordValid(password: String) -> Bool {
+        let passRegex = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$&*])(?=.*[A-Z])[^\\s]{6,}$"
+        let passwordTesting = NSPredicate(format: "SELF MATCHES %@", passRegex)
+        return passwordTesting.evaluate(with: password)
+    }
+    
+    func isEmailValid(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$"
+        let emailTesting = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return emailTesting.evaluate(with: email)
+    }
+    
+    func isValidFirstAndLastName(names: String) -> Bool {
+        guard names.count > 2 else { return false }
+        let nameRegex = "^(([^ ]?)(^[a-zA-Z].*[a-zA-Z]$)([^ ]?))$"
+        let predicateTesting = NSPredicate(format: "SELF MATCHES %@", nameRegex)
+        return predicateTesting.evaluate(with: names)
+    }
+    
+    func isValidUsername(username: String) -> Bool {
+        let usernameRegex = "^[a-zA-Z0-9]{4,}$"
+        let usernameTesting = NSPredicate(format:"SELF MATCHES %@", usernameRegex)
+        return usernameTesting.evaluate(with: username)
+    }
+    
+    func validateAge(birthDate: Date) -> Bool {
+        let maximumAge: Date = Calendar.current.date(byAdding: .year, value: -100, to: Date())!
+        let minimumAge: Date = Calendar.current.date(byAdding: .year, value: -18, to: Date())!
+        
+        var isValid: Bool = true
+        
+        if birthDate < maximumAge || birthDate > minimumAge {
+            isValid = false
+        }
+        
+        return isValid
     }
     
 }
