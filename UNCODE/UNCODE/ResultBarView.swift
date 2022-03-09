1eb18e375c947652e92fd3f4214f5252217e1c32
Aleksandar Lukic <acafineti@gmail.com>
2022-03-09 19:26:50 +0100

    custom view

--- UNCODE/UNCODE/ResultBarView.swift
+++ UNCODE/UNCODE/ResultBarView.swift
@@ -2,19 +2,71 @@
 //  ResultBarView.swift
 //  UNCODE
 //
-//  Created by aleksandar on 8.3.22..
+//  Created by aleksandar on 9.3.22..
 //
 
 import UIKit
 
 class ResultBarView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var contentView: UIView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("ResultBarView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let green = (0...15)
+        let yellow = (16...33)
+        let red = (34...48)
+        for i in green {
+            greenLine(x: CGFloat(2 + (i * 6)))
+        }
+        for i in yellow {
+            yellowLine(x: CGFloat(2 + (i * 6)))
+        }
+        for i in red {
+            redLine(x: CGFloat(2 + (i * 6)))
+        }
+    }
+    
+    func greenLine(x: CGFloat) {
+        let context = UIGraphicsGetCurrentContext()
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor(named: "Green")!.cgColor)
+        context?.move(to: CGPoint(x: x, y: 15))
+        context?.addLine(to: CGPoint(x: x, y: 40))
+        context?.strokePath()
+    }
+    
+    func yellowLine(x: CGFloat) {
+        let context = UIGraphicsGetCurrentContext()
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor(named: "Yellow")!.cgColor)
+        context?.move(to: CGPoint(x: x, y: 15))
+        context?.addLine(to: CGPoint(x: x, y: 40))
+        context?.strokePath()
+    }
+    
+    func redLine(x: CGFloat) {
+        let context = UIGraphicsGetCurrentContext()
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor(named: "Alert - red")!.cgColor)
+        context?.move(to: CGPoint(x: x, y: 15))
+        context?.addLine(to: CGPoint(x: x, y: 40))
+        context?.strokePath()
+    }
 }
